30257767eaaf4ea6bd81dace9d0806c0a5b580aa
Elbrinner <elbrinner@hotmail.com>
2017-04-04 23:54:46 +0200

    Ejemplo simples de una app con Xamarin y Android. Forma de trabajara clásica. NO RECOMENDABLE

---
+++ Android.MVC/MainActivity.cs
@@ -0,0 +1,53 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+using System;
+using Android.Content;
+
+namespace Android.MVC
+{
+    [Activity(Label = "Android.MVC", MainLauncher = true, Icon = "@drawable/icon")]
+    public class MainActivity : Activity
+    {
+        EditText username, password;
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+
+            //Set our view from the "main" layout resource
+             SetContentView (Resource.Layout.homeView);
+
+             this.username = FindViewById<EditText>(Resource.Id.user);
+             this.password = FindViewById<EditText>(Resource.Id.password);
+
+            Button button = FindViewById<Button>(Resource.Id.acceder);
+
+            button.Click += delegate {
+                new Handler(Looper.MainLooper).Post(
+              () =>
+              {
+                  if(!string.IsNullOrEmpty(username.Text) && !string.IsNullOrEmpty(password.Text))
+                  {
+                      //Navegamos
+                       StartActivity(new Intent(this, typeof(LogadoActivity)));
+                  }
+                  else
+                  {
+                      //Si es un error, mostramos un dialogo
+                         AlertDialog.Builder builder = new AlertDialog.Builder(this);
+                         AlertDialog alertdialog = builder.Create();
+
+                          alertdialog.SetMessage("Relllene los datos");
+                          alertdialog.Show();
+                  }
+                 
+
+                 
+              });
+
+            };
+        }
+
+    }
+}
+
